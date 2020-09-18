a28a21e85fdec21525dbeb699e87e43e3b85a0fe
GuroTozzi <v.alessandro.developer@gmail.com>
2020-09-18 12:39:09 +0200

    'test2'

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-const ecpress = require('express');
+// const ecpress = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static(__dirname + 'dist/angular-on-heroku'));
+// app.get('/*', function(req, res){
+//   res.sendFile(path.join(__dirname + 'dist/angular-on-heroku/index.html'));
+// });
+// app.listen(process.env.PORT || 8000);
+
+//Install express server
+const express = require('express');
+
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + 'dist/angular-on-heroku'));
-app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname + 'dist/angular-on-heroku/index.html'));
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
-app.listen(process.env.PORT || 8000);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
